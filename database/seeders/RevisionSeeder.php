e7d62d584152851e8dbda03a21601a07defe6280
Martin <martinsimongo@gmail.com>
2024-07-22 19:58:03 +0200

    Modification seeders revision main

--- database/seeders/RevisionSeeder.php
+++ database/seeders/RevisionSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Carte;
 use App\Models\Theme;
 use App\Models\Revision;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 
 class RevisionSeeder extends Seeder
 {
@@ -14,15 +15,34 @@ class RevisionSeeder extends Seeder
      */
     public function run(): void
     {
-        // Sélectionner un thème public aléatoire
-        $theme = Theme::where('public', true)->inRandomOrder()->first();
+        // Sélectionner deux thèmes publics aléatoires
+        $themes = Theme::where('public', true)->inRandomOrder()->take(2)->get();
 
-        if ($theme) {
-            $cartes = Carte::where('theme_id', $theme->id)->get();
-            foreach ($cartes as $carte) {
-                Revision::factory()->create([
-                    'carte_id' => $carte->id
-                ]);
+        if ($themes->count() > 0) {
+            foreach ($themes as $theme) {
+                $cartes = Carte::where('theme_id', $theme->id)->get();
+                $countCartes = $cartes->count();
+
+                foreach ($cartes as $index => $carte) {
+                    $niveau = rand(1, 2); // Niveaux 1 et 2 pour les révisions
+                    $dateRevision = Carbon::now();
+
+                    if ($niveau == 2) {
+                        $dateRevision = $dateRevision->addDays(2); // Niveau 2: Révision doit être faite dans 2 jours
+                    }
+
+                    // Pour les deux dernières cartes, on met la date de révision à hier
+                    if ($index >= $countCartes - 2) {
+                        $dateRevision = Carbon::yesterday();
+                        $niveau = 1; // On force le niveau à 1 pour ces cartes
+                    }
+
+                    Revision::factory()->create([
+                        'carte_id' => $carte->id,
+                        'niveau' => $niveau,
+                        'dateRevision' => $dateRevision,
+                    ]);
+                }
             }
         } else {
             echo "Aucun thème public trouvé.";
