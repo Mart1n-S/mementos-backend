cc1346784cf91dcde09debbfbb555d2e32719ddb
Martin <martinsimongo@gmail.com>
2024-07-25 12:16:59 +0200

    correction

--- app/Http/Controllers/Api/RevisionController.php
+++ app/Http/Controllers/Api/RevisionController.php
@@ -29,7 +29,7 @@ class RevisionController extends Controller
 
             // Vérifier si le thème est public ou appartient à l'utilisateur connecté
             if (!$theme->public && $theme->user_id !== $user->id) {
-                return response()->json(['error' => 'Accès refusé. Le thème et privé.'], 403);
+                return response()->json(['error' => 'Accès refusé. Le thème est privé.'], 403);
             }
 
             // Vérifier si le thème est déjà révisé par l'utilisateur
